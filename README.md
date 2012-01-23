525718c96dfe7d6129f3d9b960ca1c893d125003
Jeff Barczewski <jeff.barczewski@gmail.com>
2012-01-23 08:18:58 -0600

    complete implementation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # react-graphviz
 
-react-graphviz is a plugin for react, the flow control rules engine, which allows react to use graphviz to generate flow diagrams for the dependencies
+react-graphviz is a plugin for react, the flow control rules engine, which can use graphviz to generate flow diagrams for the dependencies
 
 For more information on `react` the lightweight flow control rules engine:  http://github.com/jeffbski/react
 
@@ -10,11 +10,13 @@ Generate graphviz diagrams for flow definitions that:
 
  - show relationship between inputs, tasks, and outputs
  - show dependencies between inputs, outputs, and other preconditions
- - create PNG files in a designated directory
+ - create PNG, PDF, GIF, or DOT files in a designated directory
+
 
 ## Installing
 
-    npm install react-graphviz
+    npm install -g react-graphviz  # installs globally
+    npm install react-graphviz   # install locally
 
 OR
 
@@ -23,19 +25,11 @@ Pull from github - http://github.com/jeffbski/react-graphviz
 
 ## Example
 
-```javascript
-var react = require('react-graphviz'); // enable graphviz integration
-react.graphFlow('outputPath'); // graph the flow for all react functions defined after this call and save to outputPath directory
-
-// OR if you just want to graph the flow for one function, you can instead do this
-
-var fn = react('myflow', ...);
-
-// if specified outputPath exists and is a directory, then the filename will be outputPath/flowName.png
-// otherwise it will be use the outputPath as the file name (directory should already exist)
-react.graphFlow(fn, 'outputPath'); // graph the existing flow for one react function and save to outputPath
+```bash
+react-graphviz moduleOrJSFile ... --type png --include flowName --output dir
+# OR
+node_modules/bin/react-graphviz moduleOrJSFile ... --type png --include flowName --output dir
 ```
-
 
 ## License
 
--- test/react-graphviz.test.js
+++ test/react-graphviz.test.js
@@ -1,10 +1,13 @@
 'use strict';
+/*jshint white: false */
 
-var tap = require('tap').test;
+var test = require('tap').test;
 var temp = require('temp');
 var path = require('path');
+var fs = require('fs');
 
-var react = require('../'); //require('react-graphviz');
+var react = require('react');
+var reactGraphviz = require('../'); //require('react-graphviz');
 
 function loadFoo(fooPath, cb) {
   setTimeout(function () {
@@ -22,17 +25,62 @@ function render(foo, bar) {
   return ['<html>', foo, '/', bar, '</html>'].join('');
 }
 
+function loadUser(uid, cb){ setTimeout(cb, 100, null, "User"+uid); }
+function loadFile(filename, cb){ setTimeout(cb, 100, null, 'Filedata'+filename); }
+function markdown(filedata) { return 'html'+filedata; }
+function prepareDirectory(outDirname, cb){ setTimeout(cb, 200, null, 'dircreated-'+outDirname); }
+function writeOutput(html, user, cb){  setTimeout(cb, 300, null, html+'_bytesWritten'); }
+function loadEmailTemplate(cb) { setTimeout(cb, 50, null, 'emailmd'); }
+function customizeEmail(user, emailHtml) { return 'cust-'+user+emailHtml; }
+function deliverEmail(custEmailHtml, cb) { setTimeout(cb, 100, null, 'delivered-'+custEmailHtml); }
 
-test('generate diagram for single flow using file path', function (t) {
+test('generate diagram with defaults', function (t) {
+  t.plan(1);
+  reactGraphviz();  //use all defaults
+  var expectedFile = './loadRender.png';
+  if (path.existsSync(expectedFile)) fs.unlinkSync(expectedFile);
   var fn = react('loadRender', 'fooPath, barPath, barP2, cb -> err, renderedOut',
     loadFoo, 'fooPath, cb -> err, foo',
     loadBar, 'barPath, barP2, cb -> err, bar',
     render, 'foo, bar -> renderedOut'
   );
+  setTimeout(function () {
+    t.ok(path.existsSync(expectedFile), 'png file should exist');
+    fs.unlinkSync(expectedFile);
+    t.end();
+  }, 1000);
+});
 
-  temp.mkdir('react-graphviz-test-single', function (err, dirPath) {
-    var filename = path.join(dirPath, 'single.png');
-    react.graphFlow(fn, filename);
-    // should this be sync or async? we would test for existence here
+test('generate diagram for single flow using file path', function (t) {
+  t.plan(3);
+  temp.mkdir('react-graphviz-test', function (err, dirPath) {
+    reactGraphviz({
+      type: 'dot',
+      output: dirPath,
+      include: ['loadAndSave', 'foo']
+    });
+    var foo = react('foo', 'a, cb -> err, b',
+      loadUser, 'a, cb -> err, b'
+    );                    
+    var bar = react('bar', 'a, cb -> err, b',
+      loadUser, 'a, cb -> err, b'
+    );                    
+    var loadAndSave = react('loadAndSave', 'filename, uid, outDir, cb -> err, html, user, bytes',  
+      loadUser,         'uid, cb          -> err, user',     // calling async loadUser with uid, cb called with err and user
+      loadFile,         'filename, cb     -> err, filedata',
+      markdown,         'filedata         -> html',    // using a sync function
+      prepareDirectory, 'outDir, cb   -> err, dircreated',
+      writeOutput,      'html, user, cb   -> err, bytes', { after: prepareDirectory },  // only after prepareDirectory done
+      loadEmailTemplate, 'cb              -> err, emailmd',
+      markdown,         'emailmd          -> emailHtml',   // using a sync function
+      customizeEmail,   'user, emailHtml  -> custEmailHtml',  // sync function
+      deliverEmail,     'custEmailHtml, cb    -> err, deliveredEmail', { after: writeOutput   }  // only after writeOutput is done
+   );
+    setTimeout(function () {
+      t.ok(path.existsSync(path.join(dirPath, 'loadAndSave.dot')), 'should exist');
+      t.ok(path.existsSync(path.join(dirPath, 'foo.dot')), 'should exist');
+      t.ok(!path.existsSync(path.join(dirPath, 'bar.dot')), 'should not exist');
+      t.end();
+    }, 1000);    
   });
 });
